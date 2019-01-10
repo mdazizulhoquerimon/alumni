1663baa626c7d30bbb0a8c4fde4372df082d9be4
rimon <mdazizulhoquerimon@gmail.com>
2019-01-10 18:04:28 +0600

    Added Admin Panel to the PATH /login

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,3 +83,25 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+
+/**** USER DEFINED CONSTANTS **********/
+
+define('ROLE_ADMIN',                            '1');
+define('ROLE_MANAGER',                         	'2');
+define('ROLE_EMPLOYEE',                         '3');
+
+define('SEGMENT',								2);
+
+/************************** EMAIL CONSTANTS *****************************/
+
+define('EMAIL_FROM',                            'Your from email');		// e.g. email@example.com
+define('EMAIL_BCC',                            	'Your bcc email');		// e.g. email@example.com
+define('FROM_NAME',                             'CIAS Admin System');	// Your system name
+define('EMAIL_PASS',                            'Your email password');	// Your email password
+define('PROTOCOL',                             	'smtp');				// mail, sendmail, smtp
+define('SMTP_HOST',                             'Your smtp host');		// your smtp host e.g. smtp.gmail.com
+define('SMTP_PORT',                             '25');					// your smtp port e.g. 25, 587
+define('SMTP_USER',                             'Your smtp user');		// your smtp user
+define('SMTP_PASS',                             'Your smtp password');	// your smtp password
+define('MAIL_PATH',                             '/usr/sbin/sendmail');
--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,38 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+
+/*********** USER DEFINED ROUTES *******************/
+
+$route['loginMe'] = 'login/loginMe';
+$route['dashboard'] = 'user';
+$route['logout'] = 'user/logout';
+$route['userListing'] = 'user/userListing';
+$route['userListing/(:num)'] = "user/userListing/$1";
+$route['addNew'] = "user/addNew";
+$route['addNewUser'] = "user/addNewUser";
+$route['editOld'] = "user/editOld";
+$route['editOld/(:num)'] = "user/editOld/$1";
+$route['editUser'] = "user/editUser";
+$route['deleteUser'] = "user/deleteUser";
+$route['profile'] = "user/profile";
+$route['profile/(:any)'] = "user/profile/$1";
+$route['profileUpdate'] = "user/profileUpdate";
+$route['profileUpdate/(:any)'] = "user/profileUpdate/$1";
+
+$route['loadChangePass'] = "user/loadChangePass";
+$route['changePassword'] = "user/changePassword";
+$route['changePassword/(:any)'] = "user/changePassword/$1";
+$route['pageNotFound'] = "user/pageNotFound";
+$route['checkEmailExists'] = "user/checkEmailExists";
+$route['login-history'] = "user/loginHistoy";
+$route['login-history/(:num)'] = "user/loginHistoy/$1";
+$route['login-history/(:num)/(:num)'] = "user/loginHistoy/$1/$2";
+
+$route['forgotPassword'] = "login/forgotPassword";
+$route['resetPasswordUser'] = "login/resetPasswordUser";
+$route['resetPasswordConfirmUser'] = "login/resetPasswordConfirmUser";
+$route['resetPasswordConfirmUser/(:any)'] = "login/resetPasswordConfirmUser/$1";
+$route['resetPasswordConfirmUser/(:any)/(:any)'] = "login/resetPasswordConfirmUser/$1/$2";
+$route['createPasswordUser'] = "login/createPasswordUser";
