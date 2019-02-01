a958d848478404d66f3958f2af4879abcc03fc1f
rimon <mdazizulhoquerimon@gmail.com>
2019-02-02 02:01:38 +0600

    Fix base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/alumni';
+//$config['base_url'] = 'http://localhost/alumni';
+$config['base_url'] = 'http://cuelsa.okkhorit.com';
 
 date_default_timezone_set('Asia/Dhaka');
 /*
