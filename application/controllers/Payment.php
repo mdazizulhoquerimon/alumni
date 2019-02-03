174ce13526c28c6e0969062602e8653d1a6b38dc
rimon <mdazizulhoquerimon@gmail.com>
2019-02-04 02:05:32 +0600

    fixes payment error

--- application/controllers/Payment.php
+++ application/controllers/Payment.php
@@ -1,6 +1,13 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Payment extends CI_Controller{
+class Payment extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('event_model');
+    }
 
 	public function monthlypayment()
 	{
