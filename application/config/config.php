c718b70f0523ecf3802bf27e70ed454c17c763e7
RIMON <mdazizulhoquerimon@gmail.com>
2019-02-13 14:56:31 +0600

    updated config and db for cuelsa.org

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/alumni';
-$config['base_url'] = 'http://www.cuelsa.org/demo/';
+$config['base_url'] = 'http://www.cuelsa.org/';
 
 date_default_timezone_set('Asia/Dhaka');
 /*
